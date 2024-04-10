6fc872659f971d125527e3a5d37ecce1a930364f
João Pedro <jaaum.p95@gmail.com>
2024-04-10 20:39:00 -0300

    :wrench: chore: configuracao inicial do app

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+
+export default function Header() {
+    return (
+     <header>
+        <div className="container">
+            <div>
+                <h1 className="text-4xl">GathSession.</h1>
+            </div>
+        </div>
+     </header>
+    );
+}
