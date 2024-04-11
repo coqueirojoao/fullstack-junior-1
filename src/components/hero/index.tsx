acde0cfc20c18defe157af90778ae6310cc61298
João Pedro <jaaum.p95@gmail.com>
2024-04-11 06:24:19 -0300

    :sparkles: feat: esboco do componente hero com a imagem de lado

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,18 +1,25 @@
 import { poppins } from "@/app/fonts";
 import Container from "../container";
+import content from '@/assets/content.svg';
+import Image from "next/image";
 
 export default function Hero() {
     return(
         <main>
             <Container>
-                <div className="mt-14">
-                    <h1 className="w-[480px] h-[100px] text-5xl text-white font-bold">
+                <div className="mt-5 flex w-full">
+                    <div className="mt-9 mr-5">
+                    <h1 className="w-[350px] h-[100px] text-4xl text-white font-bold">
                         Build your best community starting from here.
                     </h1>
-                    <h3 className={`w-[400px] mt-16 text-white font-thin opacity-50 ${poppins.className} text-sm`}>
+                    <h3 className={`w-[330px] mt-10 text-white font-thin opacity-50 ${poppins.className} text-xs`}>
                         Meet and communicate with the best people to run projects, events or other activities in a more effective and fun way
                     </h3>
-                </div>
+                    </div>
+                    <div>
+                    <Image src={content} alt="Content" />
+                    </div>
+                    </div>
             </Container>
         </main>
     )
