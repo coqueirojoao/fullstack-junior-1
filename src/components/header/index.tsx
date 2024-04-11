00733b6978feaaa2fc0979ab1e5f855f240ccbdd
João Pedro <jaaum.p95@gmail.com>
2024-04-11 08:07:21 -0300

    :ok_hand: style: adiciona borda personalizada ao botao home da navbar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     const getNavBarLinks = () => {
         return navBarLinks.map((link) => {
             return (
-                <li className={`list-none text-[8px] text-white font-light ${poppins.className}`} key={link.name}>{link.name}</li>
+                <li className={`list-none text-[8px] text-white font-light pb-[2px] cursor-pointer ${link.name === 'Home' && 'relative after:absolute after:bottom-[-2px] after:left-0 after:w-[60%] after:border-b-2 after:rounded after:border-[#DB2A6B]'} ${poppins.className}`} key={link.name}>{link.name}</li>
             )
         })
     }
@@ -17,7 +17,7 @@ export default function Header() {
 
     return (
      <header>
-        <Container height="h-[100px]">
+        <Container height="h-[60px]">
             <div className="flex w-full justify-between items-center">
                 <h1 className="text-2xl text-white">
                     <span className="text-[#DB2A6B]">Gath</span>
@@ -26,7 +26,7 @@ export default function Header() {
                     </span></h1>
                 <div className="flex gap-7 items-center">
                 { getNavBarLinks() }
-                <Image src={Menu} alt="Menu" width={15} height={15} className="ml-5"/>
+                <Image src={Menu} alt="Menu" width={15} height={15} className="ml-5 cursor-pointer"/>
                 </div>
             </div>
         </Container>
