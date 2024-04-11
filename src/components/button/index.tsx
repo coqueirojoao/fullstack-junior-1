857efcad7e4cdaac5fc39185c59824849c1fbe12
João Pedro <jaaum.p95@gmail.com>
2024-04-11 06:55:12 -0300

    :sparkles: feat: componente de botao

---
+++ src/components/button/index.tsx
@@ -0,0 +1,34 @@
+import { poppins } from "@/app/fonts";
+import Image from "next/image";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  type: string;
+  icon?: any;
+};
+
+
+
+export default function Button({ children, type, icon }: ButtonProps) {
+
+    const renderButton = () => {
+        if(type === "primary") {
+            return (
+                <button className={`${poppins.className} py-2 px-8 bg-[#DB2A6B] text-white text-[9px] rounded-ss-lg rounded-se-lg rounded-ee-lg ` }>
+                    {children}
+                </button>
+            )
+        }
+
+        return (
+            <button className={`${poppins.className} text-white text-[9px] font-semibold underline underline-offset-1 flex items-center gap-1` }>
+                {children}
+                <Image src={icon} alt="Arrow" className="w-5" />
+            </button>
+        )
+    }
+
+    return (
+       renderButton()
+    )
+}
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,7 +1,9 @@
 import { poppins } from "@/app/fonts";
 import Container from "../container";
+import Arrow from '@/assets/Arrow.svg';
 import content from '@/assets/content.svg';
 import Image from "next/image";
+import Button from "../button";
 
 export default function Hero() {
     return(
@@ -15,6 +17,14 @@ export default function Hero() {
                     <h3 className={`w-[330px] mt-10 text-white font-thin opacity-50 ${poppins.className} text-xs`}>
                         Meet and communicate with the best people to run projects, events or other activities in a more effective and fun way
                     </h3>
+                    <div className="flex mt-5 items-center gap-4">
+                    <Button type="primary">
+                        Get Started
+                    </Button>
+                    <Button type="secondary" icon={Arrow}>
+                        Learn More
+                    </Button>
+                    </div>
                     </div>
                     <div>
                     <Image src={content} alt="Content" />
